9f0c7ea1086b63ff5f3879e71db7f81edae27c95
Gledson013 <gledson.souza@hotmail.com>
2023-01-05 00:08:35 -0300

    CYPRESS AULA 3 - ARRAY ['x-b3-traceid'].to.eq

--- cypress/e2e/api/naoLogado.cy.js
+++ cypress/e2e/api/naoLogado.cy.js
@@ -5,13 +5,17 @@ describe('API - Profile', () => {
         it('valida a API de perfis', () => {
 
             cy.log('Teste de texto')
+            
             cy.request({
                 method: 'GET',
                 url: '/personalization/cl2/freeform/WebsiteDetect?source=wwwhead&fetchType=js&modalView=login'
             }).then(respostaAPI => {
                 expect(respostaAPI.status).to.eq(200) // to.eq  to equal significa igualar 
                 expect(respostaAPI.duration).to.be.lessThan(10000) // .to.be.lessThan(10000) ser menor que 10 segundos
-
+                expect(respostaAPI.headers.pragma).to.eq('no-cache')
+                expect(respostaAPI.headers.allow).to.eq('GET, POST, OPTIONS')
+                expect(respostaAPI.headers.date).to.not.be.null // Resultados esperados são objetos com letras, traços e espaços. Ao usar números ou números com letras o teste quebra
+                expect(respostaAPI.headers['x-frame-options']).to.eq('DENY')  //  Até aqui é o Chai.js e usamos o Expect www.chaijs.com
             })            
         })        
     })    
