930b3ed64b4b3c0a93c291dbf9b24a4bafaa6353
Gledson013 <gledson.souza@hotmail.com>
2023-01-08 12:06:17 -0300

    Cy['']

--- cypress/e2e/api/naoLogado.cy.js
+++ cypress/e2e/api/naoLogado.cy.js
@@ -14,8 +14,9 @@ describe('API - Profile', () => {
                 expect(respostaAPI.duration).to.be.lessThan(10000) // .to.be.lessThan(10000) ser menor que 10 segundos
                 expect(respostaAPI.headers.pragma).to.eq('no-cache')
                 expect(respostaAPI.headers.allow).to.eq('GET, POST, OPTIONS')
-                expect(respostaAPI.headers.date).to.not.be.null // Resultados esperados são objetos com letras, traços e espaços. Ao usar números ou números com letras o teste quebra
+                expect(respostaAPI.headers.date).to.not.be.null // Resultados esperados são objetos com letras, traços e espaços. Ao usar números com letras o teste quebra. 08/01/23 Necessario usar ['valor de busca'] \o/
                 expect(respostaAPI.headers['x-frame-options']).to.eq('DENY')  //  Até aqui é o Chai.js e usamos o Expect www.chaijs.com
+                expect(respostaAPI.headers['set-cookie']).to.have.lengthOf(2)
             })            
         })        
     })    
