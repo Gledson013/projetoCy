4da9453893bd4f3562749b48821b010e4098a0d4
Gledson013 <gledson.souza@hotmail.com>
2023-01-09 22:29:54 -0300

    Cypres aula 3

--- cypress/e2e/api/naoLogado.cy.js
+++ cypress/e2e/api/naoLogado.cy.js
@@ -9,15 +9,60 @@ describe('API - Profile', () => {
             cy.request({
                 method: 'GET',
                 url: '/personalization/cl2/freeform/WebsiteDetect?source=wwwhead&fetchType=js&modalView=login'
-            }).then(respostaAPI => {
-                expect(respostaAPI.status).to.eq(200) // to.eq  to equal significa igualar 
-                expect(respostaAPI.duration).to.be.lessThan(10000) // .to.be.lessThan(10000) ser menor que 10 segundos
-                expect(respostaAPI.headers.pragma).to.eq('no-cache')
-                expect(respostaAPI.headers.allow).to.eq('GET, POST, OPTIONS')
-                expect(respostaAPI.headers.date).to.not.be.null // Resultados esperados são objetos com letras, traços e espaços. Ao usar números com letras o teste quebra. 08/01/23 Necessario usar ['valor de busca'] \o/
-                expect(respostaAPI.headers['x-frame-options']).to.eq('DENY')  //  Até aqui é o Chai.js e usamos o Expect www.chaijs.com
-                expect(respostaAPI.headers['set-cookie']).to.have.lengthOf(2)
+            }).then(({ status, duration, headers }) => {
+                expect(status).to.eq(200) // to.eq  to equal significa igualar 
+                expect(duration).to.be.lessThan(10000) // .to.be.lessThan(10000) ser menor que 10 segundos
+                expect(headers.pragma).to.eq('no-cache')
+                expect(headers.allow).to.eq('GET, POST, OPTIONS')
+                expect(headers.date).to.not.be.null // Resultados esperados são objetos com letras, traços e espaços. Ao usar números com letras o teste quebra. 08/01/23 Necessario usar ['valor de busca'] \o/
+                expect(headers['x-frame-options']).to.eq('DENY')  //  Até aqui é o Chai.js e usamos o Expect www.chaijs.com
+                expect(headers['set-cookie']).to.have.lengthOf(2)
             })            
-        })        
-    })    
+        })   
+
+    })       
+    
+    context('perfil específico', () => {
+
+        it('seleciona um usuário inválido', () => {
+
+            cy.request({
+                method: 'GET',
+                url: '/api/profile/user/1',
+                failOnStatusCode: false
+            }).then(({ status, headers }) => {
+                expect(status).to.eq(421)
+                expect(headers['x-netflix.execution-time']).to.eq('0') // Estrutura implicita
+            })            
+        })
+
+        it('valida um usuário válido', () => {  // Atenção
+            cy.request({
+                method: 'GET',
+                url: '/api/profile/user/1',
+                failOnStatusCode: false
+            }).then(({ status, }) => {
+                expect(status).to.eq(421)
+                
+            })                
+
+        })
+
+        it.only('valida um usuário válido buscando na base', () => {
+            
+            cy.request({
+                method: 'GET',
+                url: '/api/profile'
+            }).then(({ body }) => {
+
+                cy.request({
+                    method: 'GET',
+                    url: `/api/profile/user/${body[1].user._id}`
+                }).then(({ status, body }) => {
+                    expect(status).to.eq(200)
+                    expect(body.status).to.eq('Outro')
+                })
+            })
+        })                
+    })
 })
